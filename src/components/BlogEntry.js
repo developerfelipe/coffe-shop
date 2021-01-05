60fd26e6369c4204e8b2d0685a617b9538a99370
Felipe Santos <joao.fmf@hotmail.com>
2021-01-05 09:16:38 -0300

    Add initial blog entry listing

---
+++ src/components/BlogEntry.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './BlogEntry.module.css'
+
+export default function BLogEntry({title, date, excerpt}){
+    return (
+        <article className={styles.blog}>
+            <h2>{title}</h2>
+            <h3>{date}</h3>
+            <p>{excerpt}</p>
+        </article>
+    );
+}
